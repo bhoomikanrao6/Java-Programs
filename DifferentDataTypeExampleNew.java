e75f4e380b498e134d7f8b2acfe9cf03e749164e
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 11:58:05 +0530

    diffdatatypenew

---
+++ DifferentDataTypeExampleNew.java
@@ -0,0 +1,10 @@
+class DifferentDataTypeExampleNew {
+   public static void main (String args[]){
+ 
+     byte b = 12;
+     System.out.println("value of b is: " + b);
+     int num;
+     num = 10;
+     System.out.println(" The value of int num is: " + num);
+     }
+}
