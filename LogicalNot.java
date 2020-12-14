92510bd16d79ba7071e1f54439d2bde7b44d94ad
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:11:42 +0530

    logicalnot

---
+++ LogicalNot.java
@@ -0,0 +1,12 @@
+class LogicalNot {
+     public static void main (String args[]){
+		 
+		 int a = 2;
+		 int b = 3;
+		 boolean Result = a!=b;
+		 
+		 System.out.println("the result is : " + Result);
+		 
+	 }
+	 
+}
