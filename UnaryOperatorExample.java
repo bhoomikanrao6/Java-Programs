1f8025782303abd309f90df8c7539ad2081de520
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:15:28 +0530

    unaryop

---
+++ UnaryOperatorExample.java
@@ -0,0 +1,36 @@
+class UnaryOperatorExample {
+     public static void main (String args[]){
+	 
+	 System.out.println("illustrating unary operation");
+	 
+	 int k = 20;
+	 int preInc = ++k;
+	 
+	 System.out.println("the value of preInc is : " + preInc);
+	 
+	 int b = 20;
+	 
+	 int postInc = k++;
+	 
+	 System.out.println("the value of postInc is : " + postInc);
+	 
+	 System.out.println("the value of k is : " + k); 
+	 
+	 
+	 int c = 20;
+	 int preDec = --k;
+	 
+	 System.out.println("the value of preDec is : " + preDec);
+	 
+	 int d = 20;
+	 
+	 int postDec = k--;
+	 
+	 System.out.println("the value of postDec is : " + postDec);
+	 
+	 System.out.println("the value of k is : " + k); 
+	 
+	 
+	 }
+  
+  }
