4b22d0c8a26e8a9e8031c14c753f5102c1c4bc5d
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:10:16 +0530

    ifcond

---
+++ IfConditionExample.java
@@ -0,0 +1,13 @@
+class IfConditionExample {
+  public static void main(String args[]){
+
+    System.out.println("program to illustrate If cond");
+	boolean ticket = true;
+	if (ticket){
+	System.out.println("allowing to watch movie");
+	System.out.println("watching movie");
+	System.out.println("completed movie");
+	}
+	System.out.println("end of the program");
+ }
+}
