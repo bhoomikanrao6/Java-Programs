3408fe8931a5e982418646c2cfb0387c1a32c78f
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:14:27 +0530

    relationalop

---
+++ RelationalOperator.java
@@ -0,0 +1,37 @@
+class RelationalOperator {
+    public static void main(String args[]){
+		
+		System.out.println("program to illustrate Relational Operator");
+		
+		int a = 10, b = 20;
+		boolean res = a<b;
+		System.out.println("the res of a<b is: " + res);
+	
+	    int c = 10, d = 20;
+        boolean value = c>d;
+		System.out.println("the value of c>d is: " + value);
+		
+		int e = 10, f = 20;
+        boolean output = e<=f;
+		System.out.println("the output of e<=f is: " + output);
+		
+		int g = 20, h = 20;
+        boolean x = g>=h;
+		System.out.println("the res of g>=h is: " + x);
+		
+		int i = 10, j = 20;
+        boolean y = i==j;
+		System.out.println("the res of i==j is: " + y);
+		
+		int k = 10, l = 20;
+        boolean z = k!=l;
+		System.out.println("the res of c>d is: " + z);
+		
+	}
+
+}
+		
+		
+		
+		
+		
