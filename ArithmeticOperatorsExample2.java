d010ee2048952975ad9e908884001844ec8412de
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 11:51:18 +0530

    arithmeticop2

---
+++ ArithmeticOperatorsExample2.java
@@ -0,0 +1,33 @@
+class ArithmeticOperatorsExample2 {
+     public static void main (String args[]){
+		 
+	    int a = 20;
+		int b = 30;
+		int c = 40;
+		int sum = a+b+c;
+		
+		System.out.println("The value of sum : " + sum);
+		
+		int sub = a-b-c;
+		
+		System.out.println("The value of sub : " + sub);
+		
+		int mul = a*b*c;
+       
+        System.out.println("The value of mul : " + mul);
+       
+	   
+	    int r = 18;
+		int s = 4;
+		int t = 2;
+        float div = r/s/t;
+
+        System.out.println("The value of div : " + div);
+
+        int mod = r%s%t;
+
+        System.out.println("The value of mod : " + mod);		
+	 
+	 }
+
+}
