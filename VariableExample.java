42c825ca11222654e8921366c54feb93a1e31889
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:16:17 +0530

    varex

---
+++ VariableExample.java
@@ -0,0 +1,12 @@
+class VariableExample{
+    public static void main (String args[]){
+	
+	System.out.println("program begins");
+	
+	int phoneNum = 987456;
+	System.out.println("The phoneNum is: ");
+	System.out.println(phoneNum);
+	
+	System.out.println("program ends");
+	}
+}
