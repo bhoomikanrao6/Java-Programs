74b70b3e0494fb188af7004214dcb3306b95d633
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:13:41 +0530

    printhello

---
+++ PrintHello.java
@@ -0,0 +1,15 @@
+class PrintHello {
+   public static void main(String args[]){
+   String name = "Bhoomika";
+
+      System.out.println("Hello " + name);
+      System.out.println("How are you " + name);
+      System.out.println("Welcome " + name);
+
+      String h = "Hello";
+      System.out.println(h);
+ 
+      char letter = 'A';
+      System.out.println(letter);
+    }
+}
