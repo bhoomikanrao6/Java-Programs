6e549a1693029eaca0ae8eb151cea00325abb41f
bhoomikanrao6 <75967244+bhoomikanrao6@users.noreply.github.com>
2020-12-14 12:17:25 +0530

    welcometoxworkz

---
+++ WelcomeToXworkz.java
@@ -0,0 +1,8 @@
+class WelcomeToXworkz {
+        public static void main(String args[]){
+                   System.out.println("Hello\n How are you?\n Welcome to xworkz\n");
+                   //System.out.println("Hello");
+                   //System.out.println("How are you?");
+                   //System.out.println("Welcome to xworkz");
+       }
+}
